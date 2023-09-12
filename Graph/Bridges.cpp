9f430cdb1dcb7cd5bbfdbd8a9d8103022389584a
Emon <schrodingersrats@gmail.com>
2023-09-12 19:31:15 +0600

    Update Bridges.cpp

--- Graph/Bridges.cpp
+++ Graph/Bridges.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e5 + 5;
+const int N = 500 + 5;
 vector<pair<int, int>> br;
 vector<int> adj[N+5];
 int low[N+5], disc[N+5], Time = 0;
@@ -11,34 +11,43 @@ void dfsBR(int vertex, int parent) {
     if (child == parent) continue;
     if (!disc[child]) {
       dfsBR(child, vertex);
-      if (disc[vertex] < low[child])
+      if (disc[vertex] < low[child])// The condition that different from "Articulation Point"
         br.push_back({vertex, child});
       low[vertex] = min(low[vertex], low[child]);
     } else
       low[vertex] = min(low[vertex], disc[child]);
   }
 }
-void BR(int m) {
-  for (int i = 1; i <= m; i++)
+void BR(int n) {
+  for (int i = 1; i <= n; i++)
     if (!disc[i])
       dfsBR(i, -1);
   for(auto ele : br)cout << ele.first << " " << ele.second << "\n";
 }
 int main() {
-    int n, m;
-    cin >> n >> m;
-    for(int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    while (tt--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        BR(n);
     }
-    BR(m);
     return 0;
 }
-// 5 5
+// 6 7
+// 0 1
 // 1 2
+// 0 2
 // 2 3
 // 3 4
+// 2 4
 // 4 5
-// 5 2
+// https://codeforces.com/blog/entry/71146
