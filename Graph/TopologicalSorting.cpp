1da0e578dbb90851aa11d0f03e39b17aaf9b354c
Emon <schrodingersrats@gmail.com>
2023-09-12 19:50:18 +0600

    Update TopologicalSorting.cpp

--- Graph/TopologicalSorting.cpp
+++ Graph/TopologicalSorting.cpp
@@ -3,35 +3,36 @@ using namespace std;
 const int N = 100 + 5;
 vector<int> G[N+5], ans;
 bool vis[N+5];
-int n, m;
 
 void dfs(int v) {
     vis[v] = true;
     for(int u : G[v]) 
-    	if(!vis[u])
+        if(!vis[u])
             dfs(u);
     ans.push_back(v);
 }
-void topologicalSort() {
+void topologicalSort(int n) {
     for(int i = 1; i <= n; ++i)
-    	if(!vis[i])
-        	dfs(i);
+        if(!vis[i])
+            dfs(i);
     reverse(ans.begin(), ans.end());
-}
-int main() {
-    cin >> n >> m;
-    for(int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        G[u].push_back(v);
-    }
-    topologicalSort();
     for(auto ele : ans)cout << ele << " ";
 }
-// 5 6
-// 1 2
-// 2 5
-// 1 4
-// 2 4
-// 4 3
-// 1 3
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;
+    tt = 1;
+    while (tt--) {
+        int n, m;
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int u, v;
+            cin >> u >> v;
+            G[u].push_back(v);
+            G[v].push_back(u);
+        }
+        topologicalSort(n);
+    }
+    return 0;
+}
