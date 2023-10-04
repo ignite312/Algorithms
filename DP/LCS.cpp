d9406e5911076877dbd569fdb5b7fc70e8864f64
Emon <schrodingersrats@gmail.com>
2023-10-04 12:43:12 +0600

    Update LCS.cpp

--- DP/LCS.cpp
+++ DP/LCS.cpp
@@ -14,14 +14,11 @@ int lcs(string &x, string &y, int n, int m) {
 */
 int lcs(string &x, string &y, int n, int m) {
     int L[m + 1][n + 1];
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= m; j++) {
-            if (i == 0 || j == 0)
-                L[i][j] = 0;
-            else if (x[i - 1] == y[j - 1])
-                L[i][j] = L[i - 1][j - 1] + 1;
-            else
-                L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= m; j++) {
+            if(i == 0 || j == 0) L[i][j] = 0;
+            else if (x[i - 1] == y[j - 1]) L[i][j] = L[i - 1][j - 1] + 1;
+            else L[i][j] = max(L[i - 1][j], L[i][j - 1]);
         }
     }
     return L[n][m];
